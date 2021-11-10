4b07acdbc72dfef827692eac370ca6028ecbb7dc
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-10 11:28:25 -0600

    added starter

---
+++ data/industries.js
@@ -0,0 +1,7 @@
+export const industries = [
+  { value: 'technology', label: 'Technology'},
+  { value: 'sales', label: 'Sales'},
+  { value: 'design', label: 'Design'},
+  { value: 'human_resources', label: 'Human Resources'},
+  { value: 'other', label: 'Other'},
+]
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function SafeSpace({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default SafeSpace;
