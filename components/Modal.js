4e4bed73cc2000aebdff1e25f551281e8a4efcf2
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-11 21:58:20 -0600

    added form for creating a company

---
+++ components/Modal.js
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function ModalComp({ btnText, title, children, func }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        {btnText}
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
+      </Modal>
+    </>
+  );
+}
