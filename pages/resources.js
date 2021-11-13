bf7a5858e90b2de9e19b7352fe0700bb83cdd0fa
Jameka Echols <jameka.echols@gmail.com>
2021-11-12 19:50:09 -0600

    added GET for resources

--- pages/resources.js
+++ pages/resources.js
@@ -4,7 +4,8 @@ import { useRouter } from "next/router";
 import Loader from "../components/Loader";
 
 export default function Resources() {
-  const [loading] = useState(true);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
@@ -13,8 +14,43 @@ export default function Resources() {
 
     if (!user) {
       router.push("/login");
+    }else {
+      getAllResources()
     }
   }, []);
 
-  return <div>{loading ? <Loader /> : 'Resources'}</div>;
+  const getAllResources = async () => {
+    try {
+      let { data: therapy, error, status} = await supabase
+        .from('therapy')
+        .select('*')
+
+      if (error && status !== 406) {
+        throw error;
+      }
+      if (data) {
+        setData(therapy);
+      }
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setLoading(false);
+    }  
+  }
+  
+  const view = () => {
+    if (loading) {
+      return <Loader />;
+    } else {
+      return data.map((d) => (
+        <div key={d.id} className='text-white'>
+          <h1>{d.name}</h1>
+          <h3>{d.therapy_type}</h3>
+          <p>{d.profile_id}</p>
+        </div>
+      ));
+    }
+  };
+
+  return <div>{view()}</div>;
 }
