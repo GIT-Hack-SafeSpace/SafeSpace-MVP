7b34aef18873f45afa881f4fc4bcc6ec4b17cc31
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-13 19:56:25 -0600

    wdith

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,12 @@ import "../styles/globals.css";
 
 function SafeSpace({ Component, pageProps }) {
   return (
-    <>
+    <div style={{width: '317px', margin: '0 auto'}}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
