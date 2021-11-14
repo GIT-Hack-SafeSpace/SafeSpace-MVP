0dd7df61d71525902dfbbb5bce9d7ec3ea398810
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-14 09:57:15 -0600

    added hack and about pages

---
+++ pages/hackathon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Hackathon() {
+  return (
+    <div>
+      <iframe
+        style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+        width='100%'
+        height='450'
+        src='https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FSW4mVA9Uo9vZq1NHaIf8Gy%2FSafe-Space-Jam%3Fnode-id%3D0%253A1'
+        allowFullScreen
+      ></iframe>
+
+      <iframe
+        style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+        width='100%'
+        height='650'
+        src='https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FtSFJwjvYJGTZ7asZ4Uwuir%2FSafeSpace-ProtoType%3Fnode-id%3D25%253A168%26scaling%3Dscale-down%26page-id%3D0%253A1%26starting-point-node-id%3D25%253A168'
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
