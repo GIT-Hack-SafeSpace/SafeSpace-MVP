bd88de35b958bb56861e9e9cd28f446f96f6a804
Brittany Garrett <brittanymgarrett@gmail.com>
2021-11-14 13:28:25 -0600

    Fixed main resource page and structuring additional pgs

--- data/resources.js
+++ data/resources.js
@@ -1,6 +1,14 @@
 export const resources = [
-    { value: 'creative_solutions', label: 'Creative Solutions'},
-    { value: 'seeking_counseling', label: 'Seeking Counseling'},
-    { value: 'exercise_release', label: 'Exercise & Release'},
-  ]
+  { value: 'creative_solutions', label: 'Creative Solutions'},
+  { value: 'seeking_counseling', label: 'Seeking Counseling'},
+  { value: 'exercise_release', label: 'Exercise & Release'},
+];
+  
+export const creative = [
+  { 
+    tags: ['tag1', 'tag2', 'tag3'],
+    content: 'skienksl',
+    image: 'https://i.postimg.cc/hjDPVQPq/colorfulicon.png'
+  }
+];
   
---
+++ pages/resources/creative.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import { supabase } from '../../utils/client';
+import { useRouter } from 'next/router';
+import Loader from '../../components/Loader';
+import MainLayout from '../../layouts/MainLayout';
+import {creative} from '../../data/resources'
+
+
+export default function Creative() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    // checking if a user is logged in. If not, redirect to login screen
+    const user = supabase.auth.user();
+
+    if (!user) {
+      router.push('/login');
+    } else {
+      setUser(user);
+      setLoading(false)
+      console.log(creative[0].tags)
+    }
+  }, []);
+
+  const view = () => {
+    if (loading) {
+      return <Loader />;
+    } else {
+      return (
+        <>
+        <div className="titleWrap" style={{display: 'flex', justifyContent: 'space-between'}}>
+          <h2><b>Creative Solutions</b></h2>
+        </div>
+
+          {creative.map((d, i) => (
+            <div key={i}>
+            <div style={{display: 'flex', justifyContent: 'flex-start'}}>
+              <div style={{height: '50px', width: '50px', marginRight: '20px'}}>
+                <img src={d.image} alt="colorul icon"/>
+              </div>
+
+              <div >
+                <h1 style={{fontSize: '20px'}}>{d.content}</h1>
+                {
+                  d.tags.map((tag,i) => {
+                   return <h3 key={i} style={{color: 'orange', fontSize: '12px'}}>{tag}</h3>
+                  })
+                }
+              </div>
+            </div>
+            <hr style={{color: 'lightgray'}} />
+            </div>
+          ))}
+    
+        </>
+      );
+    }
+  };
+
+  return <MainLayout>{view()}</MainLayout>;
+}
