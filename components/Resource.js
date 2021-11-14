34835c37e12c1a9fa979ae0d19901348f443ed37
Brittany Garrett <brittanymgarrett@gmail.com>
2021-11-13 23:42:30 -0600

    Added links to Resource component

--- components/Resource.js
+++ components/Resource.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Link from 'next/link'
 
 const ResourceLayout = styled.div`
   display: flex;
@@ -40,22 +41,32 @@ const ResourceLayout = styled.div`
 export default function ResourcePg() {
   return (
     <ResourceLayout>
-        <div className="arrangeResources">
             <div className="resourceRectangle">
-                <img className="resourceImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKmR73W1iR_RTCb2ZUDnXIOrW487S-54JWxA&usqp=CAU" alt="Happy Black Women"/>
-                <div className="bottom-left">Creative Solutions</div>
+                <Link href="/resources/creative-solutions">
+                    <a>
+                        <img className="resourceImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKmR73W1iR_RTCb2ZUDnXIOrW487S-54JWxA&usqp=CAU" alt="Happy Black Women"/>
+                        <div className="bottom-left">Creative Solutions</div>
+                    </a>
+                </Link>
             </div>
-            
+
             <div className="resourceRectangle">
-                <img className="resourceImg" src="https://www.thetemper.com/wp-content/uploads/2020/06/Black-Therapist.png-e1516484163919.jpg" alt="Black women counseling"/>
-                <div className="bottom-left">Seek Counseling</div>
+                <Link href="/resources/creative-solutions">
+                    <a>
+                        <img className="resourceImg" src="https://www.thetemper.com/wp-content/uploads/2020/06/Black-Therapist.png-e1516484163919.jpg" alt="Black women counseling"/>
+                        <div className="bottom-left">Seek Counseling</div>
+                    </a>
+                </Link>
             </div>
-            
+
             <div className="resourceRectangle">
-                <img className="resourceImg" src="https://templebethmiriam.org/wp-content/uploads/2010/01/o-BLACK-WOMAN-HAPPY-OUTSIDE-facebook.jpg" alt="Exercise & Mediation"/>
-                <div className="bottom-left">Exercise & Meditation</div>
+                <Link href="/resources/creative-solutions">
+                    <a>
+                        <img className="resourceImg" src="https://templebethmiriam.org/wp-content/uploads/2010/01/o-BLACK-WOMAN-HAPPY-OUTSIDE-facebook.jpg" alt="Exercise & Mediation"/>
+                        <div className="bottom-left">Exercise & Meditation</div>
+                    </a>
+                </Link>
             </div>
-        </div>
     </ResourceLayout>
   );
 }
