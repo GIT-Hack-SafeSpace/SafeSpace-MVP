5be73dfd75425aa79d568da826e08aad814a487f
jeressia <jeressiajaytv@gmail.com>
2021-11-15 15:24:21 -0600

    refactored tag logic to add tag names, moved tags to data file

--- components/CommunityPost.js
+++ components/CommunityPost.js
@@ -3,12 +3,14 @@ import moment from 'moment';
 
 import styled from 'styled-components';
 
-import {profilePictures} from '../data/profilePictures';
-import {reactions} from '../data/reactions';
+import { profilePictures } from '../data/profilePictures';
+import { reactions } from '../data/reactions';
+import { tagData } from '../data/tagData';
 
 
 const imageSrcs = profilePictures.map((p) => p.url);
 const reactionIcons = reactions.map((r) => r.url);
+const tagInfo = tagData;
 
 const CommunityPostStyles = styled.div`
   display: flex;
@@ -88,24 +90,19 @@ const CommunityPostStyles = styled.div`
 
 export default function CommunityPost({ data }) {
   const randomImage = () => imageSrcs[Math.floor(Math.random() * imageSrcs.length)];
-  const numOfLikes = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+  const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
   const tagColorRandomizer = () => {
-    const colors = [];
-
-    colors.push('#ED3457','#EEA127','#493843','#63988E','#000000');
-    
-    const shuffledArray = colors.sort((a, b) => 0.5 - Math.random());
-    
-    const numOfTags = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-    
-    const selected = () => shuffledArray.slice(0,numOfTags(2,4));
+    const shuffledArray = tagInfo.sort((a, b) => 0.5 - Math.random());
+    const selected = () => shuffledArray.slice(0, randomNumber(1, 3));
 
     return selected();
+  
   }
 
   const tagMaker = () => {
     let tagColors = tagColorRandomizer();
+    console.log(tagColors,'tagColors');
     return tagColors;
   }
 
@@ -116,25 +113,25 @@ export default function CommunityPost({ data }) {
       {data?.map((d) => (
         <CommunityPostStyles key={d.id}>
           <div className="main-card-body">
-          <div className="commImgWrapper">
-            <img src={randomImage()} className="profileImage" alt="image" />
-          </div>
-          <div className="card-wrapper">
-            <div className="commTitleWrapper">
-              <span className="title">{d.title}</span>
-              <span className="resolvedTag">
-                {d.isResolved ? 'Resolved' : 'Unresolved'}
-              </span>
+            <div className="commImgWrapper">
+              <img src={randomImage()} className="profileImage" alt="image" />
             </div>
-            <p className="date">{moment(d.created_at).format('MMM DD, YYYY')}</p>
-            <div className="commBody">
-              <p className="content">{d.content}</p>
-              <div className="comment-tags">
-                {tagMaker().map((i)=> <div className="tag" key={i} style={{backgroundColor: `${i}`}}>Tag</div>)}
+            <div className="card-wrapper">
+              <div className="commTitleWrapper">
+                <span className="title">{d.title}</span>
+                <span className="resolvedTag">
+                  {d.isResolved ? 'Resolved' : 'Unresolved'}
+                </span>
               </div>
-              <p>{numOfLikes(1,9)} Likes</p>
+              <p className="date">{moment(d.created_at).format('MMM DD, YYYY')}</p>
+              <div className="commBody">
+                <p className="content">{d.content}</p>
+                <div className="comment-tags">
+                  {tagMaker().map((i) => <div className="tag" key={i.id} style={{ backgroundColor: `${i.tagColor}` }}>{i.tagName}</div>)}
+                </div>
+                <p>{randomNumber(1, 9)} Likes</p>
+              </div>
             </div>
-          </div>
           </div>
           <div className="comm-footer">
             <div className="reactions">
---
+++ data/tagData.js
@@ -0,0 +1,12 @@
+export const tagData = [
+  { id: '1', tagName:'PTO', tagColor:'#ED3457' },
+  { id: '2', tagName:'Work culture', tagColor:'#EEA127' },
+  { id: '3', tagName:'Bias', tagColor:'#493843' },
+  { id: '4', tagName:'Mansplaining', tagColor:'#63988E' },
+  { id: '5', tagName:'Salary', tagColor:'#000000' },
+  { id: '6', tagName:'Hair', tagColor:'#ED3457' },
+  { id: '7', tagName:'Concrete Ceiling', tagColor:'#EEA127' },
+  { id: '8', tagName:'Promotion', tagColor:'#493843' },
+  { id: '9', tagName:'Microagression', tagColor:'#63988E' },
+  { id: '10', tagName:'Management', tagColor:'#000000' },
+]
