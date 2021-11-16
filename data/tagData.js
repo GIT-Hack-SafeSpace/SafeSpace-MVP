171994e3a244aa6f6b48ef87c9abed15a2f22f04
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-15 18:21:13 -0600

    update the data file

--- data/tagData.js
+++ data/tagData.js
@@ -1,31 +1,31 @@
 export const communityTagData = [
-  { id: '1', tagName:'PTO', tagColor:'#ED3457' },
-  { id: '2', tagName:'Work culture', tagColor:'#EEA127' },
-  { id: '3', tagName:'Bias', tagColor:'#493843' },
-  { id: '4', tagName:'Mansplaining', tagColor:'#63988E' },
-  { id: '5', tagName:'Salary', tagColor:'#000000' },
-  { id: '6', tagName:'Hair', tagColor:'#ED3457' },
-  { id: '7', tagName:'Job Hunting', tagColor:'#EEA127' },
-  { id: '8', tagName:'Promotion', tagColor:'#493843' },
-  { id: '9', tagName:'Microagression', tagColor:'#63988E' },
-  { id: '10', tagName:'Management', tagColor:'#000000' },
-]
+  { id: '1', tagName: 'PTO', tagColor: '#ED3457' },
+  { id: '2', tagName: 'Work culture', tagColor: '#EEA127' },
+  { id: '3', tagName: 'Bias', tagColor: '#493843' },
+  { id: '4', tagName: 'Mansplaining', tagColor: '#63988E' },
+  { id: '5', tagName: 'Salary', tagColor: '#000000' },
+  { id: '6', tagName: 'Hair', tagColor: '#ED3457' },
+  { id: '7', tagName: 'Job Hunting', tagColor: '#EEA127' },
+  { id: '8', tagName: 'Promotion', tagColor: '#493843' },
+  { id: '9', tagName: 'Microagression', tagColor: '#63988E' },
+  { id: '10', tagName: 'Management', tagColor: '#000000' },
+];
 
 export const communityTagOptions = [
-  { value:'PTO', label:'PTO' },
-  { value:'Work culture', label:'Work culture'},
-  { value:'Bias', label:'Bias' },
-  { value:'Mansplaining', label:'Mansplaining'},
-  { value:'Salary', label:'Salary'},
-  { value:'Hair', label:'Hair' },
-  { value:'Job Hunting', label:'Job Hunting'},
-  { value:'Promotion', label:'Promotion' },
-  { value:'Microagression', label:'Microagression'},
-  { value:'Management', label:'Management' },
-]
+  { value: 'PTO', label: 'PTO' },
+  { value: 'Work culture', label: 'Work culture' },
+  { value: 'Bias', label: 'Bias' },
+  { value: 'Mansplaining', label: 'Mansplaining' },
+  { value: 'Salary', label: 'Salary' },
+  { value: 'Hair', label: 'Hair' },
+  { value: 'Job Hunting', label: 'Job Hunting' },
+  { value: 'Promotion', label: 'Promotion' },
+  { value: 'Microagression', label: 'Microagression' },
+  { value: 'Management', label: 'Management' },
+];
 
 export const resourceTagData = [
-  { id: '1', tagName:'Exercise', tagColor:'#ED3457' },
-  { id: '2', tagName:'Meditation', tagColor:'#EEA127' },
-  { id: '3', tagName:'Cooldown', tagColor:'#493843' },
-]
+  { id: '1', tagName: 'Exercise', tagColor: '#ED3457' },
+  { id: '2', tagName: 'Meditation', tagColor: '#EEA127' },
+  { id: '3', tagName: 'Cooldown', tagColor: '#493843' },
+];
