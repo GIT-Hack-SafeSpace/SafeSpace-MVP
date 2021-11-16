922c28368f6309e3cca405e88369dffc6d1fa6a9
jeressia <jeressiajaytv@gmail.com>
2021-11-16 12:31:56 -0600

    updated footed icons, footer icon active effect, changed community reaction icons

--- components/GlobalFooter.js
+++ components/GlobalFooter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from "next/router";
+
 
 const community = 'footer/community.svg';
 const companies = 'footer/companies.svg';
@@ -9,40 +11,54 @@ const resources = 'footer/resources.svg';
 
 
 export default function GlobalFooter() {
+  const router = useRouter();
+
   return (
     <footer>
-      <div className='orderFooterOptions'>
-        <Link href='/community'>
-          <div className='footerBarOption'>
-            <p className='footerIcon'><img className='footerIcon' src={community} alt='community link'/></p>
-            <p className='optionTitle'>Community</p>
-          </div>
-        </Link>
-        <Link href='/companies'>
-          <div className='footerBarOption'>
-            <p className='footerIcon'><img className='footerIcon' src={companies} alt='companies link'/></p>
-            <p className='optionTitle'>Companies</p>
-          </div>
-        </Link>
-        <Link href='/journal'>
-          <div className='footerBarOption'>
-            <p className='footerIcon'><img className='footerIcon' src={addentry} alt='addentry link'/></p>
-            <p className='optionTitle'>Journal</p>
-          </div>
-        </Link>
-        <Link href='/inspiration'>
-          <div className='footerBarOption'>
-            <p className='footerIcon'><img className='footerIcon' src={inspiration} alt='inspiration link'/></p>
-            <p className='optionTitle'>Inspiration</p>
-          </div>
-        </Link>
-        <Link href='/resources'>
-          <div className='footerBarOption'>
-            <p className='footerIcon'><img className='footerIcon' src={resources} alt='resources link'/></p>
-            <p className='optionTitle'>Resources</p>
-          </div>
-        </Link>
-      </div>
+      <ul className="allFooterButtons">
+        <li className={router.pathname == "/community" ? "activeFooter" : ""}>
+          <Link href='/community'>
+            <div className='footerBarOption'>
+              <p className='footerIcon'>
+                <img className='footerSvg' src={community} alt='community link' />
+              </p>
+              <p className='optionTitle'>Community</p>
+            </div>
+          </Link>
+        </li>
+        <li className={router.pathname == "/companies" ? "activeFooter" : ""}>
+          <Link href='/companies'>
+            <div className='footerBarOption'>
+              <p className='footerIcon'><img className='footerSvg' src={companies} alt='companies link' /></p>
+              <p className='optionTitle'>Companies</p>
+            </div>
+          </Link>
+        </li>
+        <li className={router.pathname == "/journal" ? "activeFooter" : ""}>
+          <Link href='/journal'>
+            <div className='footerBarOption'>
+              <p className='footerIcon'><img className='footerSvg' src={addentry} alt='addentry link' /></p>
+              <p className='optionTitle'>Journal</p>
+            </div>
+          </Link>
+        </li>
+        <li className={router.pathname == "/inspiration" ? "activeFooter" : ""}>
+          <Link href='/inspiration'>
+            <div className='footerBarOption'>
+              <p className='footerIcon'><img className='footerSvg' src={inspiration} alt='inspiration link' /></p>
+              <p className='optionTitle'>Inspiration</p>
+            </div>
+          </Link>
+        </li>
+        <li className={router.pathname == "/resources" ? "activeFooter" : ""}>
+          <Link href='/resources'>
+            <div className='footerBarOption'>
+              <p className='footerIcon'><img className='footerSvg' src={resources} alt='resources link' /></p>
+              <p className='optionTitle'>Resources</p>
+            </div>
+          </Link>
+        </li>
+      </ul>
     </footer>
   );
 }
