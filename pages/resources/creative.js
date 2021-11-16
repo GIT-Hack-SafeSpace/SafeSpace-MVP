e2079235378f51e32773a261ba52eb8e24df83cc
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-15 23:40:37 -0600

    fix for creative tags

--- pages/resources/creative.js
+++ pages/resources/creative.js
@@ -48,8 +48,7 @@ export default function Creative() {
                 <div style={{ marginBottom: '-15px' }}>
                   <h1 style={{ fontSize: '20px' }}>{d.content}</h1>
                   <div className="d-flex" >
-                    {/* TODO: NEED TO UPDATE THESE WITH REAL TAGS  */}
-                    <Tags tags={resourceTagData} data={resourceTagData} />
+                    <Tags tags={resourceTagData.map((rd) => rd.tagName)} data={resourceTagData} />
                   </div>
                 </div>
               </div>
