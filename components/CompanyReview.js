d9c5da47f78495b0d3b9f4050c04d448ac58a025
Yasmeen Cole <yasmeencole1@gmail.com>
2021-11-17 13:43:06 -0600

    modified CompanyReview.js: width, height and added date. Modified InspirationPost.js, center text on image.

--- components/CompanyReview.js
+++ components/CompanyReview.js
@@ -3,6 +3,8 @@ import ModalComp from '../components/Modal';
 import CreateCompany from '../components/CreateCompany';
 import Card from 'react-bootstrap/Card';
 import styled from 'styled-components';
+import moment from "moment";
+
 
 export default function CompanyReview({data, user}) {
     return (
@@ -14,13 +16,14 @@ export default function CompanyReview({data, user}) {
             data.map((d) => (
                 <div key={d.id} className='text-white'>
                     <CardStyle>
-                    <Card className="companyCard" style={{ width: '26rem', height: '12rem'}}>
+                    <Card className="companyCard" style={{ width: '100%', height: '100%'}}>
                         <Card.Body>
                         <Card.Title>{d.name}</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted industry-title">{d.industry}</Card.Subtitle>
+                        <p className="date">{moment(d.created_at).format("MMM DD, YYYY")}</p>
                         <Card.Text>{d.content}</Card.Text>
-                        <Card.Link href="#">Card Link</Card.Link>
-                        <Card.Link href="#">Another Link</Card.Link>
+                        {/* <Card.Link href="#">See More</Card.Link> */}
+                        {/* <Card.Link href="#">Another Link</Card.Link> */}
                         </Card.Body>
                     </Card>
                     </CardStyle>
@@ -35,9 +38,17 @@ export default function CompanyReview({data, user}) {
 const CardStyle = styled.div`
 .companyCard {
     color: black;
+    margin: 2px;
 }
 
 .industry-title {
     text-transform: capitalize;
+    font-size: 13px;
 }
+
+.date {
+    font-size: 12px;
+    color: #9597a1;
+}
+
 `;
--- components/InspirationPost.js
+++ components/InspirationPost.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
 import ModalComp from '../components/Modal';
 import CreateInspo from '../components/CreateInspo';
+import styled from 'styled-components';
+import Image from 'react-bootstrap/Image';
 
 
 export default function InspirationPost({data, user}) {
@@ -13,19 +15,38 @@ export default function InspirationPost({data, user}) {
             {
             data.map((d) => (
                 <div key={d.id} className='text-white'>
-                    <div>
-                        <Card className="bg-dark text-white">
-                            <Card.Img src="https://i.pinimg.com/originals/0f/5e/3f/0f5e3ffae68a19be40d3975870be02de.png" alt="_1" height="350px" />
+                    <ImageCardStyle>
+                        <Card className="imageCard">
+                            <Card.Img class="cardImage" src="https://media.istockphoto.com/photos/teamwork-friendship-hiking-help-each-other-trust-assistance-in-of-picture-id1255203350?b=1&k=20&m=1255203350&s=170667a&w=0&h=pwRoZJaOeLy2OTg06B19QbYpOCXe3WxBROBCi-H2XWQ=" alt="_1" height="100%" width="100%"/>
                             <Card.ImgOverlay>
                                 {/* <Card.Title>Card title</Card.Title>
                                 <Card.Text>Last updated 3 mins ago</Card.Text> */}
                                 <Card.Text className="inspoCardText">{d.content}</Card.Text>
                             </Card.ImgOverlay>
                         </Card>
-                    </div>
+                    </ImageCardStyle>
+
+                    {/* <Image src="https://images.everydayhealth.com/images/healthy-living/fitness/all-about-yoga-mega-722x406.jpg" fluid /> */}
                 </div>
             )) 
         }
         </>
     )
 }
+
+const ImageCardStyle = styled.div`
+.inspoCardText {
+    text-align: center;
+    margin-top: 5rem;
+    font-size: 23px;
+}
+
+.imageCard {
+    margin: 2px;
+}
+
+.cardImage {
+    max-width: 100%;
+}
+
+`;
