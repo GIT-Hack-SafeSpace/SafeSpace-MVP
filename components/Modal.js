faa3017c168b2020f80b3f2cd8933c2e77174ccd
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-16 21:52:14 -0600

    comapny modal update

--- components/Modal.js
+++ components/Modal.js
@@ -3,12 +3,7 @@ import Button from 'react-bootstrap/Button';
 import { ButtonStyle } from '../styles/ButtonStyle';
 import Modal from 'react-bootstrap/Modal';
 
-export default function ModalComp({ title, children, func }) {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+export default function ModalComp({ title, children, handleShow, handleClose, showModal }) {
   return (
     <>
       <ButtonStyle className='d-flex justify-content-center'>
@@ -16,7 +11,7 @@ export default function ModalComp({ title, children, func }) {
           <span>&#43;</span> CREATE
         </Button>
       </ButtonStyle>
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={showModal} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
