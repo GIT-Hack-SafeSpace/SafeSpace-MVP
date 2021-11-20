08eb5de91ea2e622c2bb0fd81ef6b0a478655b49
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-19 20:12:22 -0600

    fixed broken links in all resource pages

--- pages/resources/counseling.js
+++ pages/resources/counseling.js
@@ -1,60 +1,113 @@
-import React, { useEffect, useState } from 'react';
-import { supabase } from '../../utils/client';
-import { useRouter } from 'next/router';
-import Loader from '../../components/Loader';
-import MainLayout from '../../layouts/MainLayout';
-import { counselors } from '../../data/resources';
-import Link from 'next/link';
+import React, { useEffect, useState } from "react";
+import { supabase } from "../../utils/client";
+import { useRouter } from "next/router";
+import Loader from "../../components/Loader";
+import MainLayout from "../../layouts/MainLayout";
+import { counselors } from "../../data/resources";
+import Link from "next/link";
 
 export default function Counseling() {
-    const [loading, setLoading] = useState(true);
-    const [user, setUser] = useState({});
-    const router = useRouter();
-  
-    useEffect(() => {
-      // checking if a user is logged in. If not, redirect to login screen
-      const user = supabase.auth.user();
-  
-      if (!user) {
-        router.push('/login');
-      } else {
-        setUser(user);
-        setLoading(false)
-      }
-    }, []);
-  
-    const view = () => {
-      if (loading) {
-        return <Loader />;
-      } else {
-        return (
-          <>
-            <div className="titleWrap" style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <h1 style={{marginBottom: '15px'}}>Seeking Counseling</h1>
-            </div>
-  
-            {counselors.map((d, i) => (
-              <div key={i}>
-                <div style={{display: 'flex', justifyContent: 'flex-start', marginBottom: '-10px'}}>
-                    <div>
-                        <h1 style={{fontSize: '20px', color: '#63988E'}}>{d.content}</h1>
-                      { d.address ? <p style={{color: '#123456'}}>📍 {d.address}</p> : <p></p>}
-                        <div style={{marginTop: '-10px', color: 'gray'}} className="d-flex">
-                            <p><a style={{textDecoration:'none', fontSize: '18px'}} href={`tel:+${d.phone}`}>📞 </a></p>
-                            <p style={{margin: '0 10px'}}>|</p>
-                            <Link  href={`${d.link}`}>
-                                <a style={{color: '#ED3457', fontSize: '15px', textDecoration: 'None'}}>{d.link}</a>
-                            </Link>
-                        </div>
-                        <div className="d-flex" style={{marginTop: '-10px', marginBottom: '15px'}}>
-                            {d.specialty.map((specialty, i) => {
-                              return <p key={i} style={{filter: 'drop-shadow(1px 1px 1px gray)', padding: '2px 10px', color: 'gray', border: '1px solid gray', margin: '0px 5px', backgroundColor: 'white', borderRadius: '20px', fontSize: '12px'}}>{specialty}</p>
-                            })}
-                        </div>
-                    </div>
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    // checking if a user is logged in. If not, redirect to login screen
+    const user = supabase.auth.user();
+
+    if (!user) {
+      router.push("/login");
+    } else {
+      setUser(user);
+      setLoading(false);
+    }
+  }, []);
+
+  const view = () => {
+    if (loading) {
+      return <Loader />;
+    } else {
+      return (
+        <>
+          <div
+            className="titleWrap"
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <h1 style={{ marginBottom: "15px" }}>Seeking Counseling</h1>
+          </div>
+
+          {counselors.map((d, i) => (
+            <div key={i}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  marginBottom: "-10px",
+                }}
+              >
+                <div>
+                  <h1 style={{ fontSize: "20px", color: "#63988E" }}>
+                    {d.content}
+                  </h1>
+                  {d.address ? (
+                    <p style={{ color: "#123456" }}>📍 {d.address}</p>
+                  ) : (
+                    <p></p>
+                  )}
+                  <div
+                    style={{ marginTop: "-10px", color: "gray" }}
+                    className="d-flex"
+                  >
+                    <p>
+                      <a
+                        style={{ textDecoration: "none", fontSize: "18px" }}
+                        href={`tel:+${d.phone}`}
+                      >
+                        📞{" "}
+                      </a>
+                    </p>
+                    <p style={{ margin: "0 10px" }}>|</p>
+
+                    <a
+                      href={`http://${d.link}`}
+                      target="_blank"
+                      style={{
+                        color: "#ED3457",
+                        fontSize: "15px",
+                        textDecoration: "None",
+                      }}
+                    >
+                      {d.link}
+                    </a>
+                  </div>
+                  <div
+                    className="d-flex"
+                    style={{ marginTop: "-10px", marginBottom: "15px" }}
+                  >
+                    {d.specialty.map((specialty, i) => {
+                      return (
+                        <p
+                          key={i}
+                          style={{
+                            filter: "drop-shadow(1px 1px 1px gray)",
+                            padding: "2px 10px",
+                            color: "gray",
+                            border: "1px solid gray",
+                            margin: "0px 5px",
+                            backgroundColor: "white",
+                            borderRadius: "20px",
+                            fontSize: "12px",
+                          }}
+                        >
+                          {specialty}
+                        </p>
+                      );
+                    })}
+                  </div>
                 </div>
-                <hr style={{ color: 'gray' }} />
               </div>
+              <hr style={{ color: "gray" }} />
+            </div>
           ))}
         </>
       );
