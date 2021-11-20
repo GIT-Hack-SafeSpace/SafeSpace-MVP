2f817754435400951f1cbca495c87e5c96c04eff
Brittany Garrett <brittanymgarrett@gmail.com>
2021-11-20 17:09:17 -0600

    Needed to remove unused imports

--- components/Search.js
+++ components/Search.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl'
-import { VscSearch } from 'react-icons/fa'
+
 
 export default function Search({ data, func, attrs, placeholder }) {
   const [inputValue, setInputValue] = useState('');
