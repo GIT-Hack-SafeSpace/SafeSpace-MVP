7806ab31b75de2cf7742433d8bd066fe30dac5f1
jeressia <jeressiajaytv@gmail.com>
2021-11-20 06:13:04 -0600

    added user images and socials

--- components/AboutPage.js
+++ components/AboutPage.js
@@ -1,57 +1,70 @@
-import React from 'react';
-import styled from 'styled-components';
-import BioModal from './BioModal';
-import { jeressiaSocialIcons } from '../data/socialIcons';
+import React from "react";
+import styled from "styled-components";
+import {
+  jeressiasSocialIcons,
+  jamekasSocialIcons,
+  teresasSocialIcons,
+  brittanysSocialIcons,
+} from "../data/socialIcons";
 
-const groupPhoto = '/about/safe.png';
-const jeressiasImage = './about/jeressia.jpg';
-const jeressiasIcons = jeressiaSocialIcons.map((j)=> <img className="icons" src={j.iconUrl} href={j.userUrl} alt={j.alt}/>);
+const groupPhoto = "/bio/safe.png";
+
+const jeressiasImage = "./bio/jeressia.jpg";
+const jamekasImage = "./bio/jameka.jpeg";
+const teresasImage = "./bio/teresa.jpeg";
+const brittanysImage = "./bio/brittany.jpeg";
+
+const userIcons = (array) =>
+  array.map((u) => (
+    <img className="icons" src={u.iconUrl} href={u.userUrl} alt={u.alt} />
+  ));
 
 export default function AboutPage() {
   return (
     <AboutStyles>
-      <img className="groupPhoto" src={groupPhoto} alt="group image"/>
+      <img className="groupPhoto" src={groupPhoto} alt="group image" />
       <h1>Meet the Team</h1>
       <div className="teamContainer">
         <div className="memberContainer">
-          <img className="bioImage" src={jeressiasImage} alt="Jeressia" onClick={handleShow}/>
+          <img className="bioImage" src={jeressiasImage} alt="Jeressia" />
           <div className="socials">
             <div className="name">Jeressia</div>
-            <div className="social-links">{jeressiasIcons}</div>
+            <div className="social-links">{userIcons(jeressiasSocialIcons)}</div>
           </div>
         </div>
         <div className="memberContainer">
-          <img className="bioImage" src={jeressiasImage} alt="Jeressia"/>
+          <img className="bioImage" src={jamekasImage} alt="Jeressia" />
           <div className="socials">
-            <div className="name">Jeressia</div>
-            <div className="social-links">{jeressiasIcons}</div>
+            <div className="name">Jameka</div>
+            <div className="social-links">{userIcons(jamekasSocialIcons)}</div>
           </div>
         </div>
         <div className="memberContainer">
-          <img className="bioImage" src={jeressiasImage} alt="Jeressia"/>
+          <img className="bioImage" src={teresasImage} alt="Jeressia" />
           <div className="socials">
-            <div className="name">Jeressia</div>
-            <div className="social-links">{jeressiasIcons}</div>
+            <div className="name">Teresa</div>
+            <div className="social-links">{userIcons(teresasSocialIcons)}</div>
           </div>
         </div>
         <div className="memberContainer">
-          <img className="bioImage" src={jeressiasImage} alt="Jeressia"/>
+          <img className="bioImage" src={brittanysImage} alt="Jeressia" />
           <div className="socials">
-            <div className="name">Jeressia</div>
-            <div className="social-links">{jeressiasIcons}</div>
+            <div className="name">Brittany</div>
+            <div className="social-links">
+              {userIcons(brittanysSocialIcons)}
+            </div>
           </div>
         </div>
         <div className="memberContainer">
-          <img className="bioImage" src={jeressiasImage} alt="Jeressia"/>
+          {/* <img className="bioImage" src={yasmeensImage} alt="Jeressia" /> */}
           <div className="socials">
-            <div className="name">Jeressia</div>
-            <div className="social-links">{jeressiasIcons}</div>
+            <div className="name">Yasmeen</div>
+            {/* <div className="social-links">{userIcons(yasmeensSocialIcons)}</div> */}
           </div>
         </div>
       </div>
-      
     </AboutStyles>
-  )
+  );
 }
 
 const AboutStyles = styled.div`
@@ -64,50 +77,50 @@ const AboutStyles = styled.div`
     margin-top: -25px;
   }
 
-  h1{
-    font-size:28px;
-    color:#ED3457;
+  h1 {
+    font-size: 28px;
+    color: #ed3457;
   }
 
   img {
-  object-fit: cover;
+    object-fit: cover;
   }
 
-  .bioImage{
+  .bioImage {
     height: 80px;
-  width: 80px;
-  border-radius: 100%;
+    width: 80px;
+    border-radius: 100%;
   }
 
-  .teamContainer{
+  .teamContainer {
     display: flex;
     flex-direction: column;
 
-    .memberContainer{
-      display:flex;
+    .memberContainer {
+      display: flex;
       margin-bottom: 10px;
 
-      .socials{
+      .socials {
         display: flex;
         flex-direction: column;
         justify-content: center;
         margin-left: 10px;
 
-        .name{
+        .name {
           margin-bottom: 5px;
           letter-spacing: 1px;
         }
 
-        .social-links{
+        .social-links {
           display: flex;
 
-          .icons{
+          .icons {
             width: 25px;
             height: 25px;
             margin: 0 5px;
+          }
         }
       }
     }
   }
-}
-`
+`;
--- data/socialIcons.js
+++ data/socialIcons.js
@@ -1,26 +1,89 @@
-export const jeressiaSocialIcons = [
+export const jeressiasSocialIcons = [
   {
-    id: '1',
-    iconUrl: '/about/email.svg',
-    alt: '#ED3457',
+    id: 1,
+    iconUrl: '/icons/email.svg',
+    alt: 'email Jeressia',
     userUrl: '@mailto:jeressiajaytv@gmail.com',
   },
   {
-    id: '2',
-    iconUrl: '/about/www.svg',
-    alt: '#ED3457',
+    id: 2,
+    iconUrl: '/icons/www.svg',
+    alt: 'view Jeressia\'s website',
     userUrl:'http://www.jeressia.com'
   },
   {
-    id: '3',
-    iconUrl: '/about/linkedin.svg',
-    alt: '#ED3457',
+    id: 3,
+    iconUrl: '/icons/linkedin.svg',
+    alt: 'view Jeressia\'s LinkedIn',
     userUrl: 'http://www.linkedin.com/in/jeressia',
   },
   {
-    id: '4',
-    iconUrl: '/about/github.svg',
-    alt: '#ED3457',
+    id: 4,
+    iconUrl: '/icons/github.svg',
+    alt: 'view Jeressia\'s Github',
     userUrl: 'http://www.github.com/jeressia',
+  }
+]
+
+export const jamekasSocialIcons = [
+  {
+    id: 1,
+    iconUrl: '/icons/email.svg',
+    alt: 'email Jameka',
+    userUrl: '@mailto:jameka.echols@gmail.com',
+  },
+  {
+    id: 2,
+    iconUrl: '/icons/linkedin.svg',
+    alt: 'view Jameka\'s LinkedIn',
+    userUrl: 'https://www.linkedin.com/in/jameka-echols/',
+  },
+  {
+    id: 3,
+    iconUrl: '/icons/github.svg',
+    alt: 'view Jameka\'s Github',
+    userUrl: 'https://github.com/jameka23',
+  }
+]
+
+export const teresasSocialIcons = [
+  {
+    id: 1,
+    iconUrl: '/icons/www.svg',
+    alt: 'view Teresa\'s website',
+    userUrl:'https://drteresavasquez.com'
+  },
+  {
+    id: 2,
+    iconUrl: '/icons/linkedin.svg',
+    alt: 'view Teresa\'s LinkedIn',
+    userUrl: 'https://linkedin.com/in/drteresavasquez',
+  },
+  {
+    id: 3,
+    iconUrl: '/icons/github.svg',
+    alt: 'view Teresa\'s Github',
+    userUrl: 'https://github.com/drteresavasquez',
+  }
+]
+
+export const brittanysSocialIcons = [
+  {
+    id: 1,
+    iconUrl: '/icons/email.svg',
+    alt: 'email Brittany',
+    userUrl: '@mailto:brittanymgarrett@gmail.com',
+  },
+  {
+    id: 2,
+    iconUrl: '/icons/linkedin.svg',
+    alt: 'view Brittany\'s LinkedIn',
+    userUrl: 'https://www.linkedin.com/in/brittanymgarrett/',
+  },
+  {
+    id: 3,
+    iconUrl: '/icons/github.svg',
+    alt: 'view Brittany\'s Github',
+    userUrl: 'https://github.com/bmgdevelopment',
   }
 ]
