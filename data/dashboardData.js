cd5df933c533ba19240ad83acf5bdcead4e1774b
Jameka Echols <jameka.echols@gmail.com>
2021-11-21 08:10:58 -0600

    one object for dashboard

--- data/dashboardData.js
+++ data/dashboardData.js
@@ -1,49 +1,9 @@
-export const users = [
-    {
-        type: 'users', 
-        value: '9.8 million'
-    }
-]
-
-export const lostMoney = [
-    {
-        type: 'lost of $$ due to microaggressions', 
-        value: '$450 to $550 billion'
-    }
-]
-
-export const journals = [
-    {
-        type: 'number of journals logged each day',
-        value: '410,680'
-    }
-]
-
-export const topCompanies = [
-    {
-        type:'top industries Black women are in',
-        value:'35% of Black women work in Management, Business, Science & Arts'
-    }
-]
- 
-export const numOfCompanies = [
-    {
-        type:'number of companies recommended',
-        value: '6,711'
-    }
-]
-
-
-export const climateBW = [
-    {
-        type:'current climate of Black women in US',
-        value:'meh'
-    }
-]
-
-export const topTags = [
-    {
-        type:'most journal tag',
-        value:'microaggression'
-    }
-]
+export const dashboardData = {
+    topCompanies: '35% of Black women work in Management, Business, Science & Arts',
+    numberOfCompanies: '6,711',
+    currentClimate: 'meh',
+    topTags: 'microaggression',
+    lostMoney: '$450 to $550 billion',
+    numberOfJournals: '410,680',
+    totalUsers: '9.8 million'
+}
