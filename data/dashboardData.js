4244af288a6856c2010fb70d4eca6cf55c700aa8
Jameka Echols <jameka.echols@gmail.com>
2021-11-21 07:34:45 -0600

    added data for dashboard and updated brit bio for readme and about me

---
+++ data/dashboardData.js
@@ -0,0 +1,41 @@
+export const users = [
+    {
+        type: 'users', 
+        value: '9.8 million'
+    }
+]
+
+export const lostMoney = [
+    {
+        type: 'lost of $$ due to microaggressions', 
+        value: '$450 to $550 billion'
+    }
+]
+
+export const journals = [
+    {
+        type: 'number of journals logged each day',
+        value: '410,680'
+    }
+]
+
+export const topCompanies = [
+    {
+        type:'top industries Black women are in',
+        value:'Management, business, science & arts'
+    }
+]
+
+export const climateBW = [
+    {
+        type:'current climate of Black women in US',
+        value:'meh'
+    }
+]
+
+export const topTags = [
+    {
+        type:'most journal tag',
+        value:'microaggression'
+    }
+]
