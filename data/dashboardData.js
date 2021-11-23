190a10adb8a3a92ce6a99353bca1bc4242424cd4
Jameka Echols <jameka.echols@gmail.com>
2021-11-22 22:46:56 -0600

    removing the data file

--- data/dashboardData.js
+++
@@ -1,9 +0,0 @@
-export const dashboardData = {
-    topCompanies: '35% of Black women work in Management, Business, Science & Arts',
-    numberOfCompanies: '6,711',
-    currentClimate: 'meh',
-    topTags: 'microaggression',
-    lostMoney: '$450 to $550 billion',
-    numberOfJournals: '410,680',
-    totalUsers: '9.8 million'
-}
