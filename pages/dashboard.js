7862de4fdfccf685683d90ee8233c486466edceb
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-22 21:19:08 -0600

    updated some copy and images also componetized dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,81 +1,39 @@
-import React from 'react'
-// import styled from "styled-components";
-import { Header, Statistic } from 'semantic-ui-react'
-
-const dashboardData = {
-    topCompanies: '35% of Black women work in Management, Business, Science & Arts',
-    numberOfCompanies: '6,711',
-    currentClimate: 'meh',
-    topTags: 'microaggression',
-    lostMoney: '$450 to $550 billion',
-    numberOfJournals: '410,680',
-    totalUsers: '9.8 million'
-}
-
-const HeaderExampleSubheaderProp = () => (
-    <Header
-    as='h2'
-    color='violet' 
-    textAlign='left'
-    content='SafeSpace. Dashboard'
-    subheader='Quick summarized figures'
-  />
-);
-
-const StatisticNum = () => (
-    <Statistic color='grey'>
-      <Statistic.Value>6,711</Statistic.Value>
-      <Statistic.Label>Total Companies</Statistic.Label>
-    </Statistic>
-)
-const StatisticVal = () => (
-    <Statistic size='tiny' color='grey'>
-      <Statistic.Value>#microagression</Statistic.Value>
-      <Statistic.Label>Top Tag</Statistic.Label>
-    </Statistic>
-)
-
+import router from 'next/router';
+import React, { useState } from 'react';
+import { Button, Header } from 'semantic-ui-react';
+import StatDetails from '../components/dashboard/StatDetails';
+import StatNumber from '../components/dashboard/StatNumber';
+import StatText from '../components/dashboard/StatText';
+import { dashboard } from '../data/dashboard';
+// import { supabase } from '../utils/client'; // will use when able to pull from database
 
 export default function Dashboard() {
-    return (
-        <div style={{padding: '30px', backgroundColor: '#F4F4F4'}}>
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-                <HeaderExampleSubheaderProp />
-            </div>
-                <br/>
-            <div className="divAround"> 
-                <div className="mediumBox">
-                    <div style={{padding: '2px'}}>
-                        <p style={{fontSize: '20px', fontFamily: 'Poppins'}}>Top Companies</p>
-                    </div>
-                    <hr style={{margin: '5px 0px'}}/>
-                    <div style={{display: 'flex', alignItems: 'center', fontFamily: 'Poppins', paddingLeft: '5px'}}>
-                        <p style={{fontSize: '15px', marginBottom: '0px', letterSpacing: '1px', flex: '2', marginRight: '10px'}}>This percentage reflects  Black women primarily in Management, Business, Science & Arts</p>
-                        <p style={{flex: '1', fontSize: '53px', alignContent: 'center'}}>35%</p>
-                    </div>
-                </div>
+  const { stateOfUs, users, companies, industry, revenue, topTag } = dashboard;
 
-                <div className="mediumBox" style={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Poppins', paddingLeft: '5px'}}>
-                        <StatisticNum/>
-                        <img style={{height: '90px'}} src='../footer/companies.svg'/>
-                </div>
-
-                <div className="mediumBox">
-                    <div style={{padding: '2px'}}>
-                        <p style={{fontSize: '20px', fontFamily: 'Poppins'}}>Lost Money</p>
-                    </div>
-                    <hr style={{margin: '5px 0px'}}/>
-                    <div style={{display: 'flex', alignItems: 'center', fontFamily: 'Poppins', paddingLeft: '5px'}}>
-                        <p style={{flex: '1', fontSize: '53px', alignContent: 'center', marginBottom: '0px'}}>$$$</p>
-                        <p style={{fontSize: '23px', marginBottom: '0px', letterSpacing: '1px', flex: '2', marginLeft: '10px'}}>450 to 550 Billion
-                        </p>
-                    </div>
-                </div>
-
-                <div className="mediumBox" style={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'Poppins', paddingLeft: '5px'}}>
-                        <StatisticVal/>
-                </div>
-            </div> 
-        </div> 
-    )
-};
+  return (
+    <div
+      style={{
+        padding: '30px',
+        backgroundColor: '#F4F4F4',
+        minHeight: '1200px',
+      }}
+    >
+      <Button onClick={() => router.push('/')}>{'< Back'}</Button>
+      <Header
+        as='h2'
+        color='violet'
+        textAlign='center'
+        content='SafeSpace. Impact'
+        subheader='Quick summarized figures'
+      />
+      <div className='divAround'>
+        <StatDetails object={stateOfUs} />
+        <StatNumber object={users} />
+        <StatDetails object={revenue} />
+        <StatText tag={topTag} />
+        <StatDetails object={industry} />
+        <StatNumber object={companies} />
+      </div>
+    </div>
+  );
+}
