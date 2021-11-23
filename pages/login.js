28b0e929d0219b6ae8d5a0b7ced80e54c8e25cbb
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-11-22 23:24:42 -0600

    added dashboard links

--- pages/login.js
+++ pages/login.js
@@ -6,6 +6,7 @@ import { Loader } from "../components/shared";
 import styled from "styled-components";
 import { MainButton } from "../styles/ButtonStyle";
 import Form from "react-bootstrap/Form";
+import { Button } from "semantic-ui-react";
 
 const SplashStyles = styled.div`
   padding: 300px 30px 80px;
@@ -64,11 +65,18 @@ export default function Login() {
       return (
         <main className="d-flex flex-column text-center">
           <h1 className="splash-title">SafeSpace. </h1>
+
           <hr />
           <p className="description">
             A place where Black women can support each other and manage our mental
             health and work related trauma.
           </p>
+          <div className="text-muted mt-1">
+            <div className="visually-hidden visually-hidden-focusable">
+              Learn More
+            </div>
+            <div>View <Link href="/dashboard">SafeSpace. Impact Dashboard</Link></div>
+          </div>
           <Form.Group className="form">
             <Form.Label className="visually-hidden visually-hidden-focusable">
               Email address
@@ -99,7 +107,7 @@ export default function Login() {
             <Form.Label className="visually-hidden visually-hidden-focusable">
               Learn More
             </Form.Label>
-            <Link href="/about">Learn more</Link> about SafeSpace.
+            <div><Link href="/about">Learn more</Link> about SafeSpace.</div>
           </Form.Text>
         </main>
       );
